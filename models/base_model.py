c796549c9b9da1e813fc66049d5154d9405555ad
Mcrymbo <croco2023@gmail.com>
2023-10-11 13:44:48 +0300

    storage implemented

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -20,6 +21,7 @@ class BaseModel:
         if not kwargs:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -31,7 +33,9 @@ class BaseModel:
         """
         updates public instance attribute updated_at
         """
+
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+module that serializes instances to JSON file and deserializes JSON file
+to instances
+"""
+import json
+import os
+
+
+class FileStorage:
+    """ class that serializes and deserializes instances between objects
+    and JSON file
+    """
+
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """
+        returns dictionary __objects
+        """
+
+        return self.__objects
+
+    def new(self, obj):
+        """
+        sets the obj with key <obj class name.id>
+        """
+
+        key = obj.__class__.__name__ + '.' + obj.id
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        serializes object to JSON object
+        """
+        dct = {}
+        for key, value in FileStorage.__objects.items():
+            dct[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(dct, f)
+
+    def reload(self):
+        """
+        deserializes JSON file to __objects
+        """
+        from models.base_model import BaseModel
+
+        model_class = {'BaseModel': BaseModel}
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path) as f:
+                data = json.load(f)
+                for key, value in data.items():
+                    class_name = value['__class__']
+                    m_class = model_class(class_name)
+                    if m_class:
+                        self.new(m_class(**value))
