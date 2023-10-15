761c14f2ef1f9adfd7785cc2fdf713ef01c52a7f
Mcymbo <croco2023@gmail.com>
2023-10-15 16:49:18 +0300

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -17,8 +17,8 @@ class BaseModel:
         Initializes attributes
         """
 
-        self.id = str(uuid.uuid4())
         if not kwargs:
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             storage.new(self)
@@ -27,8 +27,8 @@ class BaseModel:
                 if key == 'created_at' or key == 'updated_at':
                     value = datetime.strptime(kwargs[key],
                                               '%Y-%m-%dT%H:%M:%S.%f')
-                    if key != '__class__':
-                        setattr(self, key, value)
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
         """
